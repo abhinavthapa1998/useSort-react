6aa800b73e6915f2de8a815dd73f768f0980a75c
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-08-04 16:17:47 +0000

    Update 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import "./styles.css";
+import React from "react";
+import { SortableList } from "./components/SortableList";
+import { getInitialList } from "./data";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <SortableList data={getInitialList()} />
     </div>
   );
 }
